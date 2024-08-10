1ed488744b2060529e50b3890cc82d75cc2b303d
rudraa19 <rudra19206@gmail.com>
2024-08-10 23:40:09 +0530

    chore: update Data.jsx

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,10 +3,10 @@ const project = [
       id: 1,
       title: 'VS Code Landing Page',
       category: 'web-dev',
-      img: './images/vs-code-landingpage.png', // Update path
+      img: './images/vs-code-landingpage.png',
       desc: 'VS Code landing page using HTML and CSS (3rd Aug, 2024)',
-      github: '#',
-      link: '#',
+      github: 'https://github.com/rudraa19/cohort-3.0-projects/tree/main/vs-code-landing-page',
+      link: 'https://rudraa19.github.io/cohort-3.0-projects/vs-code-landing-page/',
     },
   ];
   
