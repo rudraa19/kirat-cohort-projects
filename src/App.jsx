92202e252ed8a8ec529958ac08b0afeb3f9ee35e
rudraa19 <rudra19206@gmail.com>
2024-08-11 17:52:31 +0530

    chore: create NotionCard for assignments

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import './App.css';
-import project from './Data'; // Make sure the path is correct
+import project from './Data'; 
 import Card from './components/Card';
 import Navbar from './components/Navbar';
+import NotionCard from './components/NotionCard';
+import assignments from './NotionData';
 
 function App() {
   return (
     <>
       <Navbar />
+
       <h2>Projects</h2>
       {project.map((proj) => (
         <Card 
           key={proj.id} title={proj.title} category={proj.category} img={proj.img} desc={proj.desc} github={proj.github} link={proj.link}
         />
       ))}
+
+      <br /><br />
+
+      <h2>Assignments</h2>
+      {assignments.map((assignment) => (
+        <NotionCard 
+          key={assignment.id} title={assignment.title} category={assignment.category} img={assignment.img} desc={assignment.desc} link={assignment.link}
+        />
+      ))}
+
+      <br /><br />
     </>
   );
 }
---
+++ src/NotionData.jsx
@@ -0,0 +1,13 @@
+const assignments = [
+    {
+      id: 1,
+      title: 'Javascript 101',
+      category: 'web-dev',
+      img: './images/assignment-1-web.png',
+      desc: 'Basics of Javascript and some assignments (4th Aug, 2024)',
+      link: 'https://band-feverfew-55d.notion.site/4-August-2024-0e798f1a1e604080a905849c88dd0024',
+    },
+  ];
+  
+  export default assignments;
+  
