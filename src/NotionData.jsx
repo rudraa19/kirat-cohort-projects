72de7f166ed5c4c483aca572970e92d59b08dfe3
rudraa19 <rudra19206@gmail.com>
2024-08-16 17:12:46 +0530

    chore: update order of NotionData.jsx

--- src/NotionData.jsx
+++ src/NotionData.jsx
@@ -1,19 +1,19 @@
 const assignments = [
     {
       id: 1,
-      title: 'Javascript 101',
-      category: 'web-dev',
-      img: './images/assignment-1-web.png',
-      desc: 'Basics of Javascript and some assignments (4th Aug, 2024)',
-      link: 'https://band-feverfew-55d.notion.site/4-August-2024-0e798f1a1e604080a905849c88dd0024',
-    },
-    {
-      id: 2,
       title: 'Web3 Cohort | Orientation',
       category: 'web3',
       img: './images/assignment-1-web3.png',
       desc: 'Intro to proof of work, Hasing using SHA256 (2nd Aug, 2024)',
       link: 'https://band-feverfew-55d.notion.site/2-August-2024-398895ce90024a8baf419acbcbcc154d',
+    },
+    {
+      id: 2,
+      title: 'Javascript 101',
+      category: 'web-dev',
+      img: './images/assignment-1-web.png',
+      desc: 'Basics of Javascript and some assignments (4th Aug, 2024)',
+      link: 'https://band-feverfew-55d.notion.site/4-August-2024-0e798f1a1e604080a905849c88dd0024',
     },
   ];
   
