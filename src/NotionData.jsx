d486365a311050ea5c2b649f21c1b40721e9c465
rudraa19 <rudra19206@gmail.com>
2024-08-16 15:31:27 +0530

    chore: update NotionCard.jsx

--- src/NotionData.jsx
+++ src/NotionData.jsx
@@ -7,6 +7,14 @@ const assignments = [
       desc: 'Basics of Javascript and some assignments (4th Aug, 2024)',
       link: 'https://band-feverfew-55d.notion.site/4-August-2024-0e798f1a1e604080a905849c88dd0024',
     },
+    {
+      id: 2,
+      title: 'Web3 Cohort | Orientation',
+      category: 'web3',
+      img: './images/assignment-1-web3.png',
+      desc: 'Intro to proof of work, Hasing using SHA256 (2nd Aug, 2024)',
+      link: 'https://band-feverfew-55d.notion.site/2-August-2024-398895ce90024a8baf419acbcbcc154d',
+    },
   ];
   
   export default assignments;
